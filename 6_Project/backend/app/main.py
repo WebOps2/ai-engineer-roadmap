67c38d0d7d26c5b9f519bd9318779d6d6558e1f7
Kosi <kosi.amobioleka@gmail.com>
2026-08-06 19:21:23 -0400

    Add allowed origin for CORS to support deployment on Render

--- 6_Project/backend/app/main.py
+++ 6_Project/backend/app/main.py
@@ -12,6 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://premier-league-rag-api.onrender.com",
     ],
     allow_methods=["*"],
     allow_headers=["*"],
